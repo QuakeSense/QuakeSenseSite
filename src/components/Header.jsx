651cdc706a4f14927b1e3b55c2bc162a7d15a25d
Hang <huhuhang@users.noreply.github.com>
2024-08-31 10:41:22 +0800

    add ph

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,6 +3,7 @@
 import Link from 'next/link'
 import { Container } from '@/components/Container'
 import { Logo } from '@/components/Logo'
+import { Button } from '@/components/Button'
 
 export function Header() {
   return (
@@ -13,6 +14,18 @@ export function Header() {
             <Link href="/" aria-label="Home">
               <Logo className="h-10 w-auto" />
             </Link>
+          </div>
+          <div className="flex items-center gap-6">
+            <div className="flex flex-col gap-4">
+              <a
+                href="https://www.producthunt.com/posts/quakesense?embed=true&utm_source=badge-featured&utm_medium=badge&utm_souce=badge-quakesense"
+                target="_blank"
+              >
+                <img
+                  src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=485460&theme=neutral"
+                />
+              </a>
+            </div>
           </div>
         </Container>
       </nav>
